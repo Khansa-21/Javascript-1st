eeb4483da44732df14a19c56083ef2339d47c5fd
Khansa-21 <khansaehsan21@gmail.com>
2024-08-07 20:13:49 +0100

    task 2

---
+++ javaScript/script.js
@@ -0,0 +1,7 @@
+var arr = ["Lahore", "Multan", "Quetta", "Muree", "Balakot"]
+var x = prompt("Enter your City name")
+if (x == arr[0] || x == arr[1] || x == arr[2] || x == arr[3] || x == arr[4]) {
+    console.log("your City is confirmed")
+} else {
+    console.log("your City is not confirmed")
+}
